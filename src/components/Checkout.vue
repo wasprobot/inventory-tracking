54353d1abe73f998bd9ed029640b785be88b9860
Rohit Wason <wasprobot@Rohits-MBP.lan1>
2019-10-04 20:41:24 -0400

    added Checkout component

---
+++ src/components/Checkout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <md-card-content>
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-small-size-100">
+          <md-field>
+            <label for="item">What are you looking for?</label>
+            <md-input name="item" id="item" v-model="form.item" />
+          </md-field>
+        </div>
+      </div>
+    </md-card-content>
+  </div>
+</template>
+
+<script scoped>
+export default {
+  name: "Checkout",
+  data: () => ({
+    form: {
+      item: null
+    }
+  })
+};
+</script>
